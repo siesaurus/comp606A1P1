f452014b3d487611ac2cfb8594c6c92c3111b3f0
siesaurus <52937221+siesaurus@users.noreply.github.com>
2019-08-08 14:08:06 +1200

    changed register.php to include validation. needs session and better design/functionality

--- Register.php
+++ Register.php
@@ -5,8 +5,35 @@
 <title>Register Now</title>
 <body>
 <?php include "nav.php"; ?>
+<div class ="container col-md-4">
+    <h2>Enter details below</h2>
+<form action="Login.php" method="post">
+    <input type="text" name="fname">
+    <input type="text" name="lName">
+    <input type="text" name="email">
+    <input type="text" name="password">
+    <input type="text" name="confirmpwd">
 
+<?php
+//defined variables and set to empty
 
+$fname = $lname = $email = $password = $confirmpwd = "";
+
+if ($_SERVER["REQUEST_METHOD"]=="POST"){
+    $fname = test_input($_POST["fname"]);
+    $lname = test_input($_POST["lname"]);
+    $email = test_input($_POST["email"]);
+    $password = test_input($_POST["password"]);
+    $confirmpwd = test_input($_POST["confirmpwd"]);
+}
+
+function test_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+?>
 
 
 <?php require_once("footer.php"); ?>
